6f7f53fca01e331b259d280356f1d41af46be622
DESKTOP-D707JML\hare <washi79washi@gmail.com>
2022-11-26 23:20:17 +0900

    初期コード

---
+++ lib/test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Test extends StatelessWidget {
+  const Test({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("testページです"),
+      ),
+    );
+  }
+}
